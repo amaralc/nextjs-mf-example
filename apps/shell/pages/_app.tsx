757c791af53f35f42383f1972869ee7fa761932a
amaralc <amaral.calil@gmail.com>
2022-11-15 19:14:21 -0300

    fix: remove emotion cache

--- apps/shell/pages/_app.tsx
+++ apps/shell/pages/_app.tsx
@@ -1,28 +1,25 @@
-import { CacheProvider } from '@emotion/react';
 import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/material/styles';
+// import { ThemeProvider } from '@mui/material/styles';
 import Head from 'next/head';
 import NavigationHeader from '../components/NavigationHeader';
-import theme from '../styles/theme';
-import createEmotionCache from '../utils/createEmotionCache';
+// import theme from '../styles/theme';
+// import createEmotionCache from '../utils/createEmotionCache';
 
 // Client-side cache, shared for the whole session of the user in the browser.
-const clientSideEmotionCache = createEmotionCache();
+// const clientSideEmotionCache = createEmotionCache();
 
 export default function MyApp(props) {
-  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  const { Component, pageProps } = props;
 
   return (
-    <CacheProvider value={emotionCache}>
+    // <CacheProvider value={emotionCache}>
+    <>
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <NavigationHeader />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </CacheProvider>
+      <CssBaseline />
+      <NavigationHeader />
+      <Component {...pageProps} />
+    </>
   );
 }
